541e83a3c9bcd5d14c0327fe0bf9a0fd44d1c576
Diana Ruth <dianaruth40@gmail.com>
2016-07-24 17:36:10 +1000

    listing of each type working correctly

--- components/scripts/controllers.js
+++ components/scripts/controllers.js
@@ -12,6 +12,7 @@ yumdbControllers.controller('RecipeSearchController', ['$scope', 'recipeSearchSe
         $("#results").hide();
         // load autocomplete search bars for ingredient inputs and enable deletion of items
         recipeSearchService.getIngredients().then(function(data) {
+            $scope.included_ingredients = [];
             var list = data.map( function (ingredient) {
                 return {
                     label: ingredient.term,
@@ -22,8 +23,18 @@ yumdbControllers.controller('RecipeSearchController', ['$scope', 'recipeSearchSe
                 source: list,
                 appendTo: $("#included-ingredients-input").next(),
                 select: function( event, ui ) {
-                    // add ingredient to list
-                    $("#included-ingredients-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + ui.item.label + '</span><span class="hidden value">' + ui.item.value + '</span></span>');
+                    // add ingredient to list if not already there
+                    var children = $("#included-ingredients-list").children();
+                    var found = false;
+                    for (var i = 1; i < children.length; i++) {
+                        if (children[i].lastElementChild.textContent == ui.item.value){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        $("#included-ingredients-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + ui.item.label + '</span><span class="hidden value">' + ui.item.value + '</span></span>');
+                    }
                     // clear text box
                     $("#included-ingredients-input").val("");
                     return false;
@@ -33,11 +44,125 @@ yumdbControllers.controller('RecipeSearchController', ['$scope', 'recipeSearchSe
                 source: list,
                 appendTo: $("#excluded-ingredients-input").next(),
                 select: function( event, ui ) {
-                    // add ingredient to list
-                    $("#excluded-ingredients-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + ui.item.label + '</span><span class="hidden value">' + ui.item.value + '</span></span>');
+                    var children = $("#excluded-ingredients-list").children();
+                    var found = false;
+                    for (var i = 1; i < children.length; i++) {
+                        if (children[i].lastElementChild.textContent == ui.item.value){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        $("#excluded-ingredients-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + ui.item.label + '</span><span class="hidden value">' + ui.item.value + '</span></span>');
+                    }
                     // clear text box
                     $("#excluded-ingredients-input").val("");
                     return false;
+                }
+            });
+        });
+        recipeSearchService.getAllergy().then(function(data) {
+            $scope.allergies = data;
+            $("#allergy-input").on("change", function() {
+                var val = this.value;
+                var text = $("#allergy-input option:selected").text();
+                var children = $("#allergy-list").children();
+                var found = false;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].lastElementChild.textContent == val){
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    $("#allergy-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + text + '</span><span class="hidden value">' + val + '</span></span>');
+                }
+            });
+        });
+        recipeSearchService.getDiet().then(function(data) {
+            $scope.diets = data;
+            $("#diet-input").on("change", function() {
+                var val = this.value;
+                var text = $("#diet-input option:selected").text();
+                var children = $("#diet-list").children();
+                var found = false;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].lastElementChild.textContent == val){
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    $("#diet-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + text + '</span><span class="hidden value">' + val + '</span></span>');
+                }
+            });
+        });
+        recipeSearchService.getCuisine().then(function(data) {
+            $scope.cuisines = data;
+            $("#included-cuisine-input").on("change", function() {
+                var val = this.value;
+                var text = $("#included-cuisine-input option:selected").text();
+                var children = $("#included-cuisine-list").children();
+                var found = false;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].lastElementChild.textContent == val){
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    $("#included-cuisine-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + text + '</span><span class="hidden value">' + val + '</span></span>');
+                }
+            });
+            $("#excluded-cuisine-input").on("change", function() {
+                var val = this.value;
+                var text = $("#excluded-cuisine-input option:selected").text();
+                var children = $("#excluded-cuisine-list").children();
+                var found = false;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].lastElementChild.textContent == val){
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    $("#excluded-cuisine-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + text + '</span><span class="hidden value">' + val + '</span></span>');
+                }
+            });
+        });
+        recipeSearchService.getCourse().then(function(data) {
+            $scope.courses = data;
+            $("#course-input").on("change", function() {
+                var val = this.value;
+                var text = $("#course-input option:selected").text();
+                var children = $("#course-list").children();
+                var found = false;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].lastElementChild.textContent == val){
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    $("#course-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + text + '</span><span class="hidden value">' + val + '</span></span>');
+                }
+            });
+        });
+        recipeSearchService.getHoliday().then(function(data) {
+            $scope.holidays = data;
+            $("#holiday-input").on("change", function() {
+                var val = this.value;
+                var text = $("#holiday-input option:selected").text();
+                var children = $("#holiday-list").children();
+                var found = false;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].lastElementChild.textContent == val){
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    $("#holiday-list").append('<span class="element"><i class="fa fa-times" onclick="$(this).parent().remove();"></i><span>' + text + '</span><span class="hidden value">' + val + '</span></span>');
                 }
             });
         });
@@ -62,5 +187,5 @@ yumdbControllers.controller('RecipeSearchController', ['$scope', 'recipeSearchSe
             $("#course-list").empty();
             $("#holiday-list").empty();
         });
-        
+
     }]);
--- components/scripts/services.js
+++ components/scripts/services.js
@@ -11,6 +11,31 @@ yumdbServices.factory('recipeSearchService', function($http) {
             return $http.get('data/ingredients.json').then(function(r) {
                 return r.data;
             });
+        },
+        getAllergy: function() {
+            return $http.get('data/allergy.json').then(function(r) {
+                return r.data;
+            });
+        },
+        getCourse: function() {
+            return $http.get('data/course.json').then(function(r) {
+                return r.data;
+            });
+        },
+        getCuisine: function() {
+            return $http.get('data/cuisine.json').then(function(r) {
+                return r.data;
+            });
+        },
+        getDiet: function() {
+            return $http.get('data/diet.json').then(function(r) {
+                return r.data;
+            });
+        },
+        getHoliday: function() {
+            return $http.get('data/holiday.json').then(function(r) {
+                return r.data;
+            });
         }
     }
 });
